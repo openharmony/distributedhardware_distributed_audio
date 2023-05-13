7ad20c8078c66ba2512590ddd60503912f185fe0
hwzhangchuang <zhangchuang.zhang@huawei.com>
2023-05-13 17:27:10 +0800

    add header content
    Signed-off-by: hwzhangchuang <zhangchuang.zhang@huawei.com>

--- common/include/daudio_ipc_interface_code.h
+++ common/include/daudio_ipc_interface_code.h
@@ -12,3 +12,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#ifndef OHOS_DAUDIO_IPC_INTERFACE_CODE_H
+#define OHOS_DAUDIO_IPC_INTERFACE_CODE_H
+
+#include <cstdint>
+
+namespace OHOS {
+namespace DistributedHardware {
+/* SAID: 4805 */
+enum class IDAudioSourceInterfaceCode : uint32_t {
+    INIT_SOURCE = 0,
+    RELEASE_SOURCE = 1,
+    REGISTER_DISTRIBUTED_HARDWARE = 2,
+    UNREGISTER_DISTRIBUTED_HARDWARE = 3,
+    CONFIG_DISTRIBUTED_HARDWARE = 4,
+    DAUDIO_NOTIFY = 5,
+};
+
+/* SAID: 4806 */
+enum class IDAudioSinkInterfaceCode : uint32_t {
+    INIT_SINK = 0,
+    RELEASE_SINK = 1,
+    SUBSCRIBE_LOCAL_HARDWARE = 2,
+    UNSUBSCRIBE_LOCAL_HARDWARE = 3,
+    DAUDIO_NOTIFY = 4,
+};
+
+/* SAID: 4805 */
+enum IDAudioIpcCBInterfaceCode : uint32_t {
+    NOTIFY_REGRESULT = 0,
+    NOTIFY_UNREGRESULT = 1,
+};
+} // namespace DistributedHardware
+} // namespace OHOS
+#endif
