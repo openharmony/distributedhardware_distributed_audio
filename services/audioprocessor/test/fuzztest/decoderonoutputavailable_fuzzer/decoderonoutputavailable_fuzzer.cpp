468bab2ccb913e42cb2e26dd23629a7474729227
w30042960 <wangyupan1@huawei.com>
2023-07-27 18:11:34 +0800

    modify FUZZ
    Signed-off-by: w30042960 <wangyupan1@huawei.com>

--- services/audioprocessor/test/fuzztest/decoderonoutputavailable_fuzzer/decoderonoutputavailable_fuzzer.cpp
+++ services/audioprocessor/test/fuzztest/decoderonoutputavailable_fuzzer/decoderonoutputavailable_fuzzer.cpp
@@ -26,7 +26,7 @@ namespace OHOS {
 namespace DistributedHardware {
 void DecoderOnOutputAvailabelFuzzTest(const uint8_t* data, size_t size)
 {
-    if (data == nullptr) {
+    if ((data == nullptr) || (size < sizeof(int64_t))) {
         return;
     }
 
