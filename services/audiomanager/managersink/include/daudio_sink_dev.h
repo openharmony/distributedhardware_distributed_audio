00f3c7807057cf16074b33075391a4d0cbda7a2d
Bobie <zhouchuanbo2@huawei.com>
2023-08-03 22:16:17 +0800

    handler 4
    Signed-off-by: Bobie <zhouchuanbo2@huawei.com>

--- services/audiomanager/managersink/include/daudio_sink_dev.h
+++ services/audiomanager/managersink/include/daudio_sink_dev.h
@@ -21,17 +21,17 @@
 #include <mutex>
 #include <initializer_list>
 
+#include "event_handler.h"
 #include "nlohmann/json.hpp"
+
 #include "daudio_sink_dev_ctrl_manager.h"
 #include "dmic_client.h"
 #include "dspeaker_client.h"
-#include "event_handler.h"
 #include "iaudio_event_callback.h"
 #include "imic_client.h"
 #include "ispk_client.h"
 #include "i_av_engine_provider.h"
 #include "i_av_receiver_engine_callback.h"
-#include "task_queue.h"
 
 #ifdef DAUDIO_SUPPORT_DIRECT
 #include "direct_dmic_client.h"
@@ -73,27 +73,7 @@ private:
     int32_t TaskSetVolume(const std::string &args);
     int32_t TaskSetMute(const std::string &args);
     int32_t TaskPlayStatusChange(const std::string &args);
-    void OnTaskResult(int32_t resultCode, const std::string &result, const std::string &funcName);
 
-    int32_t NotifyOpenCtrlChannel(const AudioEvent &audioEvent);
-    int32_t NotifyCloseCtrlChannel(const AudioEvent &audioEvent);
-    int32_t NotifyCtrlOpened(const AudioEvent &audioEvent);
-    int32_t NotifyCtrlClosed(const AudioEvent &audioEvent);
-    int32_t NotifyOpenSpeaker(const AudioEvent &audioEvent);
-    int32_t NotifyCloseSpeaker(const AudioEvent &audioEvent);
-    int32_t NotifySpeakerOpened(const AudioEvent &audioEvent);
-    int32_t NotifySpeakerClosed(const AudioEvent &audioEvent);
-    int32_t NotifyOpenMic(const AudioEvent &audioEvent);
-    int32_t NotifyCloseMic(const AudioEvent &audioEvent);
-    int32_t NotifyMicOpened(const AudioEvent &audioEvent);
-    int32_t NotifyMicClosed(const AudioEvent &audioEvent);
-    int32_t NotifySetVolume(const AudioEvent &audioEvent);
-    int32_t NotifyVolumeChange(const AudioEvent &audioEvent);
-    int32_t NotifySetParam(const AudioEvent &audioEvent);
-    int32_t NotifySetMute(const AudioEvent &audioEvent);
-    int32_t NotifyFocusChange(const AudioEvent &audioEvent);
-    int32_t NotifyRenderStateChange(const AudioEvent &audioEvent);
-    int32_t NotifyPlayStatusChange(const AudioEvent &audioEvent);
     void NotifySourceDev(const AudioEventType type, const std::string dhId, const int32_t result);
     int32_t from_json(const json &j, AudioParam &audioParam);
     int32_t HandleEngineMessage(uint32_t type, std::string content, std::string devId);
@@ -101,25 +81,22 @@ private:
     void JudgeDeviceStatus();
 
 private:
-    std::mutex taskQueueMutex_;
     std::mutex rpcWaitMutex_;
     std::condition_variable rpcWaitCond_;
     std::string devId_;
     std::string spkDhId_;
     std::string micDhId_;
-    std::shared_ptr<TaskQueue> taskQueue_ = nullptr;
     std::shared_ptr<ISpkClient> speakerClient_ = nullptr;
     std::shared_ptr<IMicClient> micClient_ = nullptr;
     std::shared_ptr<DAudioSinkDevCtrlMgr> audioCtrlMgr_ = nullptr;
 
-    using DAudioSinkDevFunc = int32_t (DAudioSinkDev::*)(const AudioEvent &audioEvent);
-    std::map<AudioEventType, DAudioSinkDevFunc> memberFuncMap_;
     std::atomic<bool> isSpkInUse_ = false;
     std::atomic<bool> isMicInUse_ = false;
 
     class SinkEventHandler : public AppExecFwk::EventHandler {
     public:
-        SinkEventHandler(const std::shared_ptr<AppExecFwk::EventRunner> &runner, const std::shared_ptr<DAudioSinkDev> &dev);
+        SinkEventHandler(const std::shared_ptr<AppExecFwk::EventRunner> &runner,
+            const std::shared_ptr<DAudioSinkDev> &dev);
         ~SinkEventHandler() override;
         void ProcessEvent(const AppExecFwk::InnerEvent::Pointer &event) override;
 
@@ -147,7 +124,7 @@ private:
     private:
         using SinkEventFunc = void (SinkEventHandler::*)(const AppExecFwk::InnerEvent::Pointer &event);
         std::map<uint32_t, SinkEventFunc> mapEventFuncs_;
-        std::shared_ptr<DAudioSinkDev> sinkDev_;
+        std::weak_ptr<DAudioSinkDev> sinkDev_;
     };
     std::shared_ptr<SinkEventHandler> handler_;
 };
