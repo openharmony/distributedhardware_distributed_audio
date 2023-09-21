dd106b61ee8475ace64312523398e7b80561edce
wujinpeng <wujinpeng5@huawei.com>
2023-09-21 13:59:20 +0800

    UT问题修改
    Signed-off-by: wujinpeng <wujinpeng5@huawei.com>

--- services/audiomanager/test/unittest/sourcedevice/src/daudio_source_dev_test.cpp
+++ services/audiomanager/test/unittest/sourcedevice/src/daudio_source_dev_test.cpp
@@ -155,7 +155,7 @@ HWTEST_F(DAudioSourceDevTest, CreatTasks_003, TestSize.Level1)
 {
     sourceDev_->AwakeAudioDev();
     AudioEvent event = AudioEvent(OPEN_SPEAKER, "");
-    EXPECT_NE(ERR_DH_AUDIO_SA_OPEN_CTRL_FAILED, sourceDev_->HandleOpenDSpeaker(event));
+    EXPECT_EQ(DH_SUCCESS, sourceDev_->HandleOpenDSpeaker(event));
 
     event.type = OPEN_MIC;
     EXPECT_NE(ERR_DH_AUDIO_SA_OPEN_CTRL_FAILED, sourceDev_->HandleOpenDMic(event));
@@ -844,7 +844,7 @@ HWTEST_F(DAudioSourceDevTest, TaskOpenCtrlChannel_001, TestSize.Level1)
     sourceDev_->rpcResult_ = true;
     sourceDev_->rpcNotify_ = sourceDev_->EVENT_NOTIFY_OPEN_MIC;
     jParam = { { KEY_DH_ID, DH_ID_SPK } };
-    EXPECT_EQ(DH_SUCCESS, sourceDev_->TaskOpenCtrlChannel(jParam.dump()));
+    EXPECT_NE(DH_SUCCESS, sourceDev_->TaskOpenCtrlChannel(jParam.dump()));
 }
 
 /**
