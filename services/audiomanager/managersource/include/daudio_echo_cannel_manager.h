642e3c2d69eecea604e1e5575a23a1656dc03666
hwwuhaobo <wuhaobo2@huawei.com>
2024-04-22 21:26:55 +0800

    add echo cannel manager
    Signed-off-by: hwwuhaobo <wuhaobo2@huawei.com>

---
+++ services/audiomanager/managersource/include/daudio_echo_cannel_manager.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DAUDIO_ECHO_CANNEL_MANAGER_H
+#define OHOS_DAUDIO_ECHO_CANNEL_MANAGER_H
+
+#include <queue>
+
+#include "single_instance.h"
+
+#include "audio_capturer.h"
+#include "audio_info.h"
+#include "aec_effect_processor.h"
+#include "iaudio_datatrans_callback.h"
+
+#include "audio_data.h"
+#include "audio_data_ext.h"
+#include "audio_param.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+class DAudioEchoCannelManager : public AudioStandard::AudioCaptureReadCallback,
+    public std::enable_shared_from_this<DAudioEchoCannelManager> {
+public:
+    DAudioEchoCannelManager();
+    ~DAudioEchoCannelManager();
+
+    int32_t SetUp(const AudioCommonParam param,
+        const std::shared_ptr<IAudioDataTransCallback> &callback);
+    int32_t Start();
+    int32_t Stop();
+    int32_t Release();
+
+    int32_t OnMicDataReceived(const std::shared_ptr<AudioData> &pipeInData);
+
+private:
+    void OnReadData(size_t length) override;
+    void AecProcessData();
+    int32_t ProcessMicData(const std::shared_ptr &pipeInData,
+        std::shared_ptr<AudioData> &micOutData);
+    
+    int32_t AudioCaptureSetUp();
+    int32_t AudioCaptureStart();
+    int32_t AudioCaptureStop();
+    int32_t AudioCaptureRelease();
+
+    int32_t LoadAecProcessor();
+    void UnLoadAecProcessor();
+    int32_t InitAecProcessor();
+    int32_t StartAecProcessor();
+    int32_t StopAecProcessor();
+    int32_t ReleaseAecProcessor();
+
+    std::unique_ptr<AudioStandard::AudioCapturer> audioCapturer = nullptr;
+    std::atomic<bool> isAecRunnig_ = false;
+    std::thread aecProcessThread_;
+    static constexpr const char* AECTHREADNAME = "AecProcessThread";
+
+    std::shared_ptr<IAudioDataTransCallback> devCallback_;
+    void *aecHandler_ = nullptr;
+    AecEffectProcessor *aecProcessor_ = nullptr;
+    constexpr static size_t COND_WAIT_TIME_MS = 10;
+    constexpr static size_t COND_WAIT_TIME_MS = 10;
+    constexpr static size_t WAIT_MIC_DATA_TIME_US = 5000;
+    constexpr static size_t REF_QUEUE_MAX_SIZE = 10;
+    std::queue<std::shared_ptr<AudioData>> refDataQueue_;
+    std::queue<std::shared_ptr<AudioData>> outDataQueue_;
+    std::mutex refQueueMtx_;
+    std::mutex outQueueMtx_;
+    std::condition_variable refQueueCond_;
+    std::atomic<bool> isStarted = false;
+} // OHOS
+#endif // OHOS_DAUDIO_DMIC_DEV_H
