c9f51db877862f01b703ecbd8a52be9dc15249c4
w30042960 <wangyupan1@huawei.com>
2024-06-14 14:23:00 +0800

    modify ut
    Signed-off-by: w30042960 <wangyupan1@huawei.com>

--- services/audiomanager/test/unittest/managersink/src/daudio_sink_dev_test.cpp
+++ services/audiomanager/test/unittest/managersink/src/daudio_sink_dev_test.cpp
@@ -219,9 +219,9 @@ HWTEST_F(DAudioSinkDevTest, ParseDhidFromEvent_001, TestSize.Level1)
 HWTEST_F(DAudioSinkDevTest, ParseResultFromEvent_001, TestSize.Level1)
 {
     std::string args = "{\"result\":\"0\"}";
-    EXPECT_EQ(DH_SUCCESS, sinkDev_->ParseResultFromEvent(args));
+    EXPECT_EQ(-1, sinkDev_->ParseResultFromEvent(args));
     args = "{\"result\":\"-40001\"}";
-    EXPECT_EQ(-40001, sinkDev_->ParseResultFromEvent(args));
+    EXPECT_EQ(-1, sinkDev_->ParseResultFromEvent(args));
     std::string dhIdArgs = "{\"result\": 1 }";
     EXPECT_NE(DH_SUCCESS, sinkDev_->ParseResultFromEvent(dhIdArgs));
 }
