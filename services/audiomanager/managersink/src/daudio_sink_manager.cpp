647800a6e2f4a2d7ab064e4aa1c6fbc33174b46e
chenminghui <minghui.chen@archermind.com>
2024-09-14 12:37:49 +0800

    单号：#IANGIF 描述:distributed_audio解耦device_security_level 问题根因：distributed_audio解耦device_security_level 解决方案：通过编译控制宏解耦device_security_level 是否完成变成规范自检：Y 是否编译且验证通过：Y 影响的设备与平台范围：rk3568
    Signed-off-by: chenminghui <minghui.chen@archermind.com>
    Change-Id: Iab0f1cf94a2d5a5ebdec1ad0c42b4ba5a2add281

--- services/audiomanager/managersink/src/daudio_sink_manager.cpp
+++ services/audiomanager/managersink/src/daudio_sink_manager.cpp
@@ -415,7 +415,7 @@ bool DAudioSinkManager::CheckDeviceSecurityLevel(const std::string &srcDeviceId,
 
 int32_t DAudioSinkManager::GetDeviceSecurityLevel(const std::string &udid)
 {
-    #ifdef DEVICE_SECURITY_LEVEL_ENABLE
+#ifdef DEVICE_SECURITY_LEVEL_ENABLE
     DeviceIdentify devIdentify;
     devIdentify.length = DEVICE_ID_MAX_LEN;
     int32_t ret = memcpy_s(devIdentify.identity, DEVICE_ID_MAX_LEN, udid.c_str(), DEVICE_ID_MAX_LEN);
@@ -431,9 +431,9 @@ int32_t DAudioSinkManager::GetDeviceSecurityLevel(const std::string &udid)
         info = nullptr;
         return DEFAULT_DEVICE_SECURITY_LEVEL;
     }
-    #endif
+#endif
     int32_t level = 0;
-    #ifdef DEVICE_SECURITY_LEVEL_ENABLE
+#ifdef DEVICE_SECURITY_LEVEL_ENABLE
     ret = GetDeviceSecurityLevelValue(info, &level);
     DHLOGE("Get device security level, level is %d", level);
     FreeDeviceSecurityInfo(info);
@@ -442,7 +442,7 @@ int32_t DAudioSinkManager::GetDeviceSecurityLevel(const std::string &udid)
         DHLOGE("Get device security level failed %d", ret);
         return DEFAULT_DEVICE_SECURITY_LEVEL;
     }
-    #endif
+#endif
     return level;
 }
 
