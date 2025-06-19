0aa8e84bc6db2a35666ab0f1065af158a21b073e
BrainL <libuyan1@huawei.com>
2025-06-19 22:19:08 +0800

    add FUZZ
    Signed-off-by: BrainL <libuyan1@huawei.com>

--- services/audiomanager/test/fuzztest/sourceservicedaudionotify_fuzzer/sourceservicedaudionotify_fuzzer.cpp
+++ services/audiomanager/test/fuzztest/sourceservicedaudionotify_fuzzer/sourceservicedaudionotify_fuzzer.cpp
@@ -19,6 +19,7 @@
 #include <cstdint>
 #include <fuzzer/FuzzedDataProvider.h>
 
+#include "daudio_constants.h"
 #include "daudio_source_service.h"
 #include "if_system_ability_manager.h"
 #include "iservice_registry.h"
@@ -43,6 +44,68 @@ void SourceServiceDAudioNotifyFuzzTest(const uint8_t* data, size_t size)
 
     dAudioSourceService->DAudioNotify(devId, dhId, eventType, eventContent);
 }
+
+void SourceServiceOnStartFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < sizeof(int32_t)) {
+        return;
+    }
+    FuzzedDataProvider fdp(data, size);
+
+    int32_t saId = fdp.ConsumeIntegral<int32_t>();
+    bool runOnCreate = fdp.ConsumeBool();
+
+    auto dAudioSourceService = std::make_shared<DAudioSourceService>(saId, runOnCreate);
+    dAudioSourceService->OnStart();
+}
+
+void SourceServiceOnStopFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < sizeof(int32_t)) {
+        return;
+    }
+    FuzzedDataProvider fdp(data, size);
+
+    int32_t saId = fdp.ConsumeIntegral<int32_t>();
+    bool runOnCreate = fdp.ConsumeBool();
+
+    auto dAudioSourceService = std::make_shared<DAudioSourceService>(saId, runOnCreate);
+    dAudioSourceService->OnStop();
+}
+
+void SourceServiceInitFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < sizeof(int32_t)) {
+        return;
+    }
+    FuzzedDataProvider fdp(data, size);
+
+    int32_t saId = fdp.ConsumeIntegral<int32_t>();
+    bool runOnCreate = fdp.ConsumeBool();
+
+    auto dAudioSourceService = std::make_shared<DAudioSourceService>(saId, runOnCreate);
+    dAudioSourceService->Init();
+}
+
+void SourceServiceDumpFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return;
+    }
+    FuzzedDataProvider fdp(data, size);
+
+    int32_t fd = fdp.ConsumeIntegral<int32_t>();
+    size_t argsCount = fdp.ConsumeIntegralInRange<size_t>(0, 10);
+    std::vector<std::u16string> args;
+    for (size_t i = 0; i < argsCount; ++i) {
+        std::string utf8Str = fdp.ConsumeRandomLengthString(100);
+        std::u16string utf16Str(utf8Str.begin(), utf8Str.end());
+        args.emplace_back(utf16Str);
+    }
+
+    auto dAudioSourceService = std::make_shared<DAudioSourceService>(DISTRIBUTED_HARDWARE_AUDIO_SOURCE_SA_ID, true);
+    dAudioSourceService->Dump(fd, args);
+}
 }
 }
 
@@ -51,6 +114,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
     OHOS::DistributedHardware::SourceServiceDAudioNotifyFuzzTest(data, size);
+    OHOS::DistributedHardware::SourceServiceOnStartFuzzTest(data, size);
+    OHOS::DistributedHardware::SourceServiceOnStopFuzzTest(data, size);
+    OHOS::DistributedHardware::SourceServiceInitFuzzTest(data, size);
+    OHOS::DistributedHardware::SourceServiceDumpFuzzTest(data, size);
     return 0;
 }
 
