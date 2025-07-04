f63ba3b244808b1562f9626f4f3e329d06b9d50b
liuxiaowei42 <liuxiaowei42@huawei.com>
2025-07-04 10:34:32 +0800

    modify FUZZ
    Signed-off-by: liuxiaowei42 <liuxiaowei42@huawei.com>

--- services/audiomanager/test/fuzztest/sourceservicedump_fuzzer/sourceservicedump_fuzzer.cpp
+++ services/audiomanager/test/fuzztest/sourceservicedump_fuzzer/sourceservicedump_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,36 +13,34 @@
  * limitations under the License.
  */
 
-#include "sourceservicedaudionotify_fuzzer.h"
+#include "sourceservicedump_fuzzer.h"
 
 #include <cstddef>
 #include <cstdint>
 #include <fuzzer/FuzzedDataProvider.h>
 
-#include "daudio_constants.h"
 #include "daudio_source_service.h"
-#include "if_system_ability_manager.h"
-#include "iservice_registry.h"
 
 namespace OHOS {
 namespace DistributedHardware {
-void SourceServiceDAudioNotifyFuzzTest(const uint8_t* data, size_t size)
+void SourceServiceDumpFuzzTest(const uint8_t* data, size_t size)
 {
-    if ((data == nullptr) || (size < (sizeof(int32_t)))) {
+    if (data == nullptr || size == 0) {
         return;
     }
     FuzzedDataProvider fdp(data, size);
-    
-    std::string dhId = fdp.ConsumeRandomLengthString();
-    int32_t saId = fdp.ConsumeIntegral<int32_t>();
-    bool runOnCreate = fdp.ConsumeBool();
-    int32_t eventType = fdp.ConsumeIntegral<int32_t>();
-    std::string eventContent = fdp.ConsumeRandomLengthString();
-    std::string devId = fdp.ConsumeRandomLengthString();
 
-    auto dAudioSourceService = std::make_shared<DAudioSourceService>(saId, runOnCreate);
+    int32_t fd = fdp.ConsumeIntegral<int32_t>();
+    size_t argsCount = fdp.ConsumeIntegralInRange<size_t>(0, 10);
+    std::vector<std::u16string> args;
+    for (size_t i = 0; i < argsCount; ++i) {
+        std::string utf8Str = fdp.ConsumeRandomLengthString(100);
+        std::u16string utf16Str(utf8Str.begin(), utf8Str.end());
+        args.emplace_back(utf16Str);
+    }
 
-    dAudioSourceService->DAudioNotify(devId, dhId, eventType, eventContent);
+    auto dAudioSourceService = std::make_shared<DAudioSourceService>(fd, true);
+    dAudioSourceService->Dump(fd, args);
 }
 }
 }
@@ -51,7 +49,7 @@ void SourceServiceDAudioNotifyFuzzTest(const uint8_t* data, size_t size)
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
-    OHOS::DistributedHardware::SourceServiceDAudioNotifyFuzzTest(data, size);
+    OHOS::DistributedHardware::SourceServiceDumpFuzzTest(data, size);
     return 0;
 }
 
