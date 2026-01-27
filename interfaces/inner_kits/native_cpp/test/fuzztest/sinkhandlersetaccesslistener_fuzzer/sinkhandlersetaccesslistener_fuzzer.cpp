565229ed9bd35d244f6f1feff64b8b197830cbb0
BrainL_ <libuyan1@huawei.com>
2026-01-27 11:40:50 +0800

    fix&fuzz
    Signed-off-by: BrainL_ <libuyan1@huawei.com>

--- interfaces/inner_kits/native_cpp/test/fuzztest/sinkhandlersetaccesslistener_fuzzer/sinkhandlersetaccesslistener_fuzzer.cpp
+++ interfaces/inner_kits/native_cpp/test/fuzztest/sinkhandlersetaccesslistener_fuzzer/sinkhandlersetaccesslistener_fuzzer.cpp
@@ -22,7 +22,7 @@ namespace OHOS {
 namespace DistributedHardware {
 void SinkHandlerSetAccessListenerFuzzTest(const uint8_t* data, size_t size)
 {
-    if (data == nullptr) {
+    if ((data == nullptr) || (size < (sizeof(int32_t)))) {
         return;
     }
     sptr<IAccessListener> listener(new TestAccessListener());
